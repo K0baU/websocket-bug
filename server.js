d18ac81514a6622a60c7c069f27cc6c727e88e2c
K0baU <63403741+K0baU@users.noreply.github.com>
2023-09-17 16:08:56 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const cons = {};
+let con;
 Deno.serve({
     port: 443,
     cert: await Deno.readTextFile("/etc/letsencrypt/live/wab.sabae.cc/fullchain.pem"),
@@ -7,30 +7,18 @@ Deno.serve({
     const upgrade = req.headers.get("upgrade") || "";
     if (upgrade.toLowerCase() == "websocket") {
         const { socket, response } = Deno.upgradeWebSocket(req);
-        socket.onmessage = async (e) => {
-            // "hello"が出力されない
-            console.log(e.data.slice(0, 8));
-            const payload = JSON.parse(e.data);
-            switch (payload.q) {
-                // クライアントがIDを名乗った
-                case "did":
-                    cons[payload.body] = socket;
-                    break;
-                // ピアのIDの一覧を要求された
-                case "neighbours":
-                    socket.send(JSON.stringify({ type: "res", body: Object.keys(cons) }));
-                    break;
-                // offer/answerの転送を要求された
-                case "transport":
-                    try { cons[payload.body.to].send(payload.body.payload); } catch (err) { };
-                    break;
+        socket.onopen = () => {
+            if (con) {
+                con.send("hi");
             }
+            con = socket;
         };
+        socket.onmessage = (e) => {
+            console.log(e.data);
+        }
         return response;
     } else {
-        let path = (new URL(req.url)).pathname;
-        if (path == "/") path = "/index.html";
-        const data = await Deno.readTextFile("client" + path);
-        return new Response(data, { headers: new Headers({ "Content-Type": path.split(".").pop() == "html" ? "text/html" : "text/javascript" }) });
+        const data = await Deno.readTextFile("index.html");
+        return new Response(data, { headers: new Headers({ "Content-Type": "text/html" }) });
     }
 });
